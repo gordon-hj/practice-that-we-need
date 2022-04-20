120534dbb0c813be6928552295f7771e5e6a8e3e
gordon.hj <gordon_hj@naver.com>
2022-04-21 00:27:54 +0900

    시퀀스와 리스트 차이 테스트코드

--- 20220419-kotlin-sequence-list/src/test/kotlin/SequenceAndListTest.kt
+++ 20220419-kotlin-sequence-list/src/test/kotlin/SequenceAndListTest.kt
@@ -4,7 +4,7 @@ import kotlin.system.measureTimeMillis
 
 class SequenceAndListTest {
     @Test
-    fun streamAndCollection() {
+    fun collectionAndSequence_showStep() {
         val arr = (1..5)
 
         val collection = arr.toList()
@@ -28,163 +28,147 @@ class SequenceAndListTest {
     }
 
     @Test
-    fun streamAndList() {
-        val list = (1..50).toList()
-        val stream = list.asSequence()
+    fun collectionAndSequence_countOperation() {
+        val arr = (1..50)
+        val resultSize = 2
 
-        var countList = 0
-        val listResult = list
-            .map {
-                countList++;
-                println("1. map : $it")
-                it
-            }
-            .filter {
-                countList++;
-                println("2. filter : $it")
-                (it % 2) == 0
-            }
-            .map {
-                countList++;
-                println("3. map : $it")
-                it
-            }
-            .filter {
-                countList++;
-                println("4. filter : $it")
-                (it % 5) == 0
-            }
-            .map {
-                countList++;
-                println("5. map : $it")
-                it
-            }
-            .take(2)
-        println(listResult)
-        println(countList)
-        println("------------------------------------------------------------------------------------------------------------------------")
-        var countStream = 0
-        val streamResult = stream
-            .map {
-                countStream++;
-                println("1. map : $it")
-                it
-            }
-            .filter {
-                countStream++;
-                println("2. filter : $it")
-                (it % 5) == 0
-            }
-            .map {
-                countStream++;
-                println("3. map : $it")
-                it
-            }
-            .filter {
-                countStream++;
-                println("4. filter : $it")
-                (it % 2) == 0
-            }
-            .map {
-                countStream++;
-                println("5. map : $it")
-                it
-            }
-            .take(2)
+        val collection = arr.toList()
+        val sequence = arr.asSequence()
+
+        var collectionCount = 0
+        val collectionResult = collection
+            .map { it.also { collectionCount++ } }
+            .filter { v -> (v % 2 == 0).also { collectionCount++ } }
+            .map { it.also { collectionCount++ } }
+            .filter { v -> (v % 5 == 0).also { collectionCount++ } }
+            .map { it.also { collectionCount++ } }
+            .take(resultSize)
+        println("Collection result : $collectionResult, operation count: $collectionCount")
+        // ====================================================================================
+        var sequenceCount = 0
+        val sequenceResult = sequence
+            .map { it.also { sequenceCount++ } }
+            .filter { v -> (v % 2 == 0).also { sequenceCount++ } }
+            .map { it.also { sequenceCount++ } }
+            .filter { v -> (v % 5 == 0).also { sequenceCount++ } }
+            .map { it.also { sequenceCount++ } }
+            .take(resultSize)
             .toList()
-        println(streamResult)
-        println(countStream)
-        println("------------------------------------------------------------------------------------------------------------------------")
-        var resultList = mutableListOf<Int>()
-        for (n in list) {
-            val t = pseudoStream(n)
-            if (t != null) {
-                resultList.add(t)
-            }
-            if (resultList.size == 2) break
-        }
-        println(resultList)
-        println(count)
+        println("Sequence result : $sequenceResult, operation count: $sequenceCount")
     }
 
-    var count: Int = 0
-    fun pseudoStream(n: Int): Int? {
-        println("1. map $n").also { count++ }
-        println("2. filter $n").also { count++ }
-        if (n % 5 != 0) return null
-        println("3. map $n").also { count++ }
-        println("4. filter $n").also { count++ }
-        if (n % 2 != 0) return null
-        println("5. map $n").also { count++ }
-        return n
+    @Test
+    fun collectionAndSequence_measureTime_smallCase() {
+        val arr = (1..5000)
+        val resultSize = 2
+
+        val collection = arr.toList()
+        val sequence = arr.asSequence()
+
+        var collectionCount = 0
+        val collectionTime = measureTimeMillis {
+            collection
+                .map { it.also { collectionCount++ } }
+                .filter { v -> (v % 2 == 0).also { collectionCount++ } }
+                .map { it.also { collectionCount++ } }
+                .filter { v -> (v % 5 == 0).also { collectionCount++ } }
+                .map { it.also { collectionCount++ } }
+                .take(resultSize)
+        }
+        println("Collection spent time: $collectionTime, operation count: $collectionCount")
+        // ====================================================================================
+        var sequenceCount = 0
+        val sequenceTime = measureTimeMillis {
+            sequence
+                .map { it.also { sequenceCount++ } }
+                .filter { v -> (v % 2 == 0).also { sequenceCount++ } }
+                .map { it.also { sequenceCount++ } }
+                .filter { v -> (v % 5 == 0).also { sequenceCount++ } }
+                .map { it.also { sequenceCount++ } }
+                .take(resultSize)
+                .toList()
+
+        }
+        println("Sequence spent time: $sequenceTime, operation count: $sequenceCount")
+    }
+
+    @Test
+    fun collectionAndSequence_measureTime_largeCase() {
+        val arr = (1..25000)
+        val resultSize = 150
+
+        val collection = arr.toList()
+        val sequence = arr.asSequence()
+
+        var collectionCount = 0
+        val collectionTime = measureTimeMillis {
+            collection
+                .map { it.also { collectionCount++ } }
+                .filter { v -> (v % 2 == 0).also { collectionCount++ } }
+                .map { it.also { collectionCount++ } }
+                .filter { v -> (v % 5 == 0).also { collectionCount++ } }
+                .map { it.also { collectionCount++ } }
+                .take(resultSize)
+        }
+        println("Collection spent time: $collectionTime, operation count: $collectionCount")
+        // ====================================================================================
+        var sequenceCount = 0
+        val sequenceTime = measureTimeMillis {
+            sequence
+                .map { it.also { sequenceCount++ } }
+                .filter { v -> (v % 2 == 0).also { sequenceCount++ } }
+                .map { it.also { sequenceCount++ } }
+                .filter { v -> (v % 5 == 0).also { sequenceCount++ } }
+                .map { it.also { sequenceCount++ } }
+                .take(resultSize)
+                .toList()
+
+        }
+        println("Sequence spent time: $sequenceTime, operation count: $sequenceCount")
     }
 
 
     @Test
-    fun streamAndList2() {
-        // 숫자 적을 땐 list가 더 나음
-        val list = (1..10000L).toList()
-        val stream = list.asSequence()
-        val size = 100000
+    fun collectionAndSequence_latteIsBest() {
+        val arr = (1..20000000)
+        val resultSize = 150
 
-        var listResult: List<Long>
-        val listTime = measureTimeMillis {
-            listResult = list
-                .filter { (it % 2) == 0L }
-                .filter { (it % 5) == 0L }
-                .take(size)
+        val collection = arr.toList()
+        val sequence = arr.asSequence()
+
+        val collectionTime = measureTimeMillis {
+            collection
+                .map { it }
+                .filter { v -> (v % 2 == 0) }
+                .map { it }
+                .filter { v -> (v % 5 == 0) }
+                .map { it }
+                .take(resultSize)
         }
-
-        var stream1Result: List<Long>
-        val stream1Time = measureTimeMillis {
-            stream1Result = stream
-                .filter { (it % 2) == 0L }
-                .filter { (it % 5) == 0L }
-                .take(size)
+        println("Collection spent time: $collectionTime")
+        // ====================================================================================
+        val sequenceTime = measureTimeMillis {
+            sequence
+                .map { it }
+                .filter { v -> (v % 2 == 0) }
+                .map { it }
+                .filter { v -> (v % 5 == 0) }
+                .map { it }
+                .take(resultSize)
                 .toList()
-        }
 
-        var stream2Result: List<Long>
-        val stream2Time = measureTimeMillis {
-            stream2Result = stream
-                .filter { (it % 2) == 0L }
-                .filter { (it % 5) == 0L }
-                .take(size)
-                .toList()
         }
-
-        var directList: MutableList<Long> = mutableListOf()
-        val directListTime = measureTimeMillis {
-            for (n in list) {
-                if (n % 2 != 0L) continue
-                if (n % 5 != 0L) continue
-                directList.add(n)
-                if (directList.size == size) break
+        println("Sequence spent time: $sequenceTime")
+        // ====================================================================================
+        val latteTime = measureTimeMillis {
+            val resultList: MutableList<Int> = mutableListOf()
+            for (n in arr) {
+                if (n % 2 != 0) continue
+                if (n % 5 != 0) continue
+                resultList.add(n)
+                if (resultList.size == resultSize) break
             }
         }
-
-        var directList2: MutableList<Long> = mutableListOf()
-        val directListTime2 = measureTimeMillis {
-            for (n in list) {
-                if (n % 5 != 0L && n % 2 != 0L) continue
-                directList2.add(n)
-                if (directList2.size == size) break
-            }
-        }
-
-        var directList3: MutableList<Long> = mutableListOf()
-        val directListTime3 = measureTimeMillis {
-            for (n in list) {
-                if (n % 2 != 0L && n % 5 != 0L) continue
-                directList3.add(n)
-                if (directList3.size == size) break
-            }
-        }
-
-        println(listResult == stream1Result)
-        println(stream1Result == stream2Result)
-        println(stream2Result == directList)
-        println(directList == directList2)
-        println("list $listTime\nstream1 $stream1Time\nstream2 $stream2Time\ndirectTime $directListTime\ndirectTime2 $directListTime2\ndirectTime3 $directListTime3")
+        println("Old-way spent time: $latteTime")
     }
 }
